e35308fd5ee1335839978a6cb7c12c859ff44e86
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-30 07:49:41 -0800

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,15 @@
+// Marvish Chandra
+
+public class dashboardRequirements{
+    func websiteEvaluators(formSubmissions,downloads,signups,purchases){
+        formEntry = "Complete the following questions to gauge our product's use.";
+        downloadTime = "The time to finish the download will approximately take a few minutes.";
+        signupIncentive = "Get a discount on our product after registering yourself as a user!";
+        purchaseCost = Int;
+    }
+    func performanceMetrics(sessions,pageviews,conversionRate){
+        sessionTime = "After the user uses your product or website, use cookies to track how long they are on there.";
+        websiteViews = "Determine the daily, weekly, monthly, and yearly views your website maintains.";
+        conversion = conversionRate * 100;
+    }
+}
